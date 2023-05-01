0b3a6d9b12fa0511c1804d4642e2ddc29f7dc5e0
Nikolay <perin.nikolay@gmail.com>
2023-05-01 18:27:22 +0300

    Project folders structure changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
-const gulp = require( 'gulp' ),
-	browserSync = require( 'browser-sync' ),
-	server = browserSync.create(),
-	localHost = 'http://localhost/starter-bootstrap';
-
+	import gulp from 'gulp';
+	import dartSass from 'sass';
+	import gulpSass from 'gulp-sass';
+	import postcss from 'gulp-postcss';
+	import sourcemaps from 'gulp-sourcemaps';
+	import autoprefixer from 'autoprefixer';
+	import cssnano from 'cssnano';
+	import imagemin, {gifsicle, mozjpeg, optipng, svgo} from 'gulp-imagemin';
+	import browserSync from 'browser-sync';
+	const sass = gulpSass(dartSass);
+	const server = browserSync.create();
+	const localHost = 'http://localhost:8000/';
 
 /**
  * Define all source paths
@@ -10,18 +17,36 @@ const gulp = require( 'gulp' ),
 
 var paths = {
 	styles: {
-		src: './assets/*.scss',
-		dest: './assets/dist'
+		src: './source/css/*.scss',
+		dest: './css/'
 	},
 	scripts: {
-		src: './assets/*.js',
-		dest: './assets/dist'
+		src: './source/js/*.js',
+		dest: './js/'
+	},
+	php: {
+		src: './source/php/*.php',
+		dest: './'
+	},
+	img: {
+		src: './source/img/**/*.{gif,jpg,jpeg,png,svg}',
+		dest: './img/'
 	}
 };
 
+function build_php() {
+	return gulp.src( paths.php.src )
+		.pipe(
+			gulp.dest( paths.php.dest )
+		)
+		.pipe(
+			server.stream() // Browser Reload
+		);
+}
+
 
 /**
- * Webpack compilation: http://webpack.js.org, https://github.com/shama/webpack-stream#usage-with-gulp-watch
+ * Webpack compilation: https://webpack.js.org, https://github.com/shama/webpack-stream#usage-with-gulp-watch
  * 
  * build_js()
  */
@@ -41,9 +66,9 @@ function build_js() {
 		.pipe(
 			gulp.dest( paths.scripts.dest )
 		)
-		/*.pipe(
+		.pipe(
 			server.stream() // Browser Reload
-		)*/;
+		);
 }
 
 
@@ -54,17 +79,11 @@ function build_js() {
  */
 
 function build_css() {
-	const sass = require( 'gulp-sass' )( require( 'sass' ) ),
-		postcss = require( 'gulp-postcss' ),
-		sourcemaps = require( 'gulp-sourcemaps' ),
-		autoprefixer = require( 'autoprefixer' ),
-		cssnano = require( 'cssnano' );
-	
 	const plugins = [
 		autoprefixer(),
 		cssnano(),
 	];
-	
+
 	return gulp.src( paths.styles.src )
 		.pipe(
 			sourcemaps.init()
@@ -84,25 +103,76 @@ function build_css() {
 		.pipe(
 			gulp.dest( paths.styles.dest )
 		)
-		/*.pipe(
+		.pipe(
 			server.stream() // Browser Reload
-		)*/;
+		);
 }
+
+function minify_img() {
+	return gulp.src( paths.img.src )
+	.pipe(
+		imagemin(
+			[
+				gifsicle( {interlaced: true} ),
+				mozjpeg( {quality: 75, progressive: true} ),
+				optipng( {optimizationLevel: 5} ),
+				svgo( {
+					plugins: [
+						{
+							name: 'removeViewBox',
+							active: true
+						},
+						{
+							name: 'cleanupIDs',
+							active: false
+						}
+					]
+				} )
+			]
+		) 
+	)
+	.pipe(
+		gulp.dest( paths.img.dest )
+		)
+	.pipe(
+		server.stream() // Browser Reload
+	);
+}
+
 
 /**
  * Watch task: Webpack + SASS
  * 
- * $ gulp watch
+ * $ gulp default
  */
 
-gulp.task( 'watch',
+gulp.task( 'default',
+	function () {
+		// Modify "localHost" constant and uncomment "server.init()" to use browser sync
+		server.init({
+			proxy: localHost,
+		} );
+		
+		gulp.watch( paths.scripts.src, build_js );
+		gulp.watch( [ paths.styles.src, './source/css/*.scss' ], build_css );
+		gulp.watch( paths.php.src, build_php );
+		gulp.watch( paths.img.src, minify_img );
+	}
+);
+
+gulp.task( 'build',
 	function () {
 		// Modify "localHost" constant and uncomment "server.init()" to use browser sync
 		/*server.init({
 			proxy: localHost,
 		} );*/
 
-		gulp.watch( paths.scripts.src, build_js );
-		gulp.watch( [ paths.styles.src, './assets/scss/*.scss' ], build_css );
+		gulp.build( paths.scripts.src, build_js );
+		gulp.build( [ paths.styles.src, './source/css/*.scss' ], build_css );
+		gulp.watch( paths.php.src, build_php );
+		gulp.watch( paths.img.src, minify_img );
 	}
 );
+
+  // export default gulp.default;
+  // export function gulp.build;
